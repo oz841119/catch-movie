a62025953928e1c0641fe5c531313f7b335caa77
oz841119 <oz841119@gmail.com>
2022-04-23 03:10:46 +0800

    feat: Search Page(未完)

--- src/views/SearchMovie.vue
+++ src/views/SearchMovie.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="searchMovie">
-    <ul class="listWrap">
+    <div class="title">{{searchContent}}</div>
+    <ul class="movieList">
       <li class="movieWrap" v-for="movie in movieDataList">
         <div class="imgWrap">
           <img :src="`https://image.tmdb.org/t/p/w185/${movie.poster_path}`" alt="">
         </div>
+        <div class="movieName">{{movie.title}}</div>
       </li>
     </ul>
   </div>
@@ -28,22 +30,36 @@ export default {
       .then(APIres => {
         const movieApiDataArr = APIres.data.results
         movieApiDataArr.forEach((movie) => {
-          movieDataList.push(movie)
+          movie.backdrop_path && movieDataList.push(movie) // 沒有海報則不成為搜尋結果
         })
         console.log(movieDataList);
       })
     })
-    return { movieDataList }
+    return { movieDataList,searchContent }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .searchMovie {
-    .listWrap {
+    width: 100%;
+    padding: 0 60px 0 60px;
+    padding-top: 50px;
+    background-color: var(--bg-main-color);
+
+    .title {
+      font-size: var(--title-main-size);
+      color: var(--title-main-color);
+      margin-bottom: var(--title-main-mb)
+    }
+    .movieList {
       display: flex;
+      flex-wrap: wrap;
+      margin-top: 40px;
       .movieWrap {
-        min-width: 185px;
+        width: 185px;
+        margin-right: 28px;
+        margin-bottom: 40px;
 
         .imgWrap {
           width: 185px;
@@ -53,6 +69,14 @@ export default {
             height: 100%;
             width: 100%;
           }
+        }
+
+        .movieName {
+          font-size: var(--movie-name-size);
+          margin-top: var(--movie-name-mt);
+          color: #fff;
+          text-align: center;
+          word-wrap:break-word;
         }
       }
     }
