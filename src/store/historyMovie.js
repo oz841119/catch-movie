6892a36055d9c4d2216fb81f2ea0b9ad04d61084
oz841119 <oz841119@gmail.com>
2022-06-07 19:18:38 +0800

    fix: 保存歷史瀏覽紀錄的vuex state有了正確的刪減或增加邏輯 響應式尚未做好，需要重寫整個Call API的邏輯及組件渲染

--- src/store/historyMovie.js
+++ src/store/historyMovie.js
@@ -10,8 +10,17 @@ export default {
     },
 
     setHistoryMovieIdList(state, movieID) {
+      console.log(state.historyMovieIdList);
       console.log(state.historyMovieIdList.indexOf(movieID));
-      if(state.historyMovieIdList.indexOf(movieID) === -1) {
+      if(state.historyMovieIdList.indexOf(movieID) === -1) { // 如果目前列表中未含有該movieID，則添加 (主要避免重複添加)
+        state.historyMovieIdList.unshift(movieID)
+        window.localStorage.setItem('historyMovieIdList',JSON.stringify(state.historyMovieIdList))
+        this.commit('getHistoryMovieIdList')
+      }
+
+      if(state.historyMovieIdList.indexOf(movieID) > 0) { // 如果列表中已含有movieID且不在首位(index == 0)時，則將該ID移至首位
+        const currentIndex = state.historyMovieIdList.indexOf(movieID)
+        state.historyMovieIdList.splice(currentIndex, 1)
         state.historyMovieIdList.unshift(movieID)
         window.localStorage.setItem('historyMovieIdList',JSON.stringify(state.historyMovieIdList))
         this.commit('getHistoryMovieIdList')
@@ -21,7 +30,6 @@ export default {
     cleanHistory(state) {
       window.localStorage.removeItem('historyMovieIdList');
       state.historyMovieIdList.length = 0
-      // console.log(state.historyMovieIdList);
     }
   },
   actions: {
